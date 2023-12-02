b5519e8e247d57b34480e3488bdf9714b0cc5461
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-12-02 19:36:37 -0300

    feat: add config loader

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	DbDriver       string
+	DbDsn          string
+	MigrationsPath string
+	DataPath       string
+}
+
+var cfg Config
+
+func init() {
+	dbDriver, ok := os.LookupEnv("DB_DRIVER")
+	if !ok || dbDriver == "" {
+		log.Fatal("env var DB_DRIVER is required")
+	}
+
+	dbDsn, ok := os.LookupEnv("DB_DSN")
+	if !ok || dbDsn == "" {
+		log.Fatal("env var DB_DSN is required")
+	}
+
+	migrationsPath, ok := os.LookupEnv("MIGRATIONS_PATH")
+	if !ok || migrationsPath == "" {
+		log.Fatal("env var MIGRATIONS_PATH is required")
+	}
+
+	dataPath, ok := os.LookupEnv("DATA_PATH")
+	if !ok || dataPath == "" {
+		log.Fatal("env var DATA_PATH is required")
+	}
+
+	cfg = Config{
+		DbDriver:       dbDriver,
+		DbDsn:          dbDsn,
+		MigrationsPath: migrationsPath,
+		DataPath:       dataPath,
+	}
+}
+
+func GetConfig() Config {
+	return cfg
+}
